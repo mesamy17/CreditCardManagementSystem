c1d291bbed81478f08c2117a0dca4941b0ff0b65
Samarth Sawant <sawant.samarths@gmail.com>
2026-06-29 13:25:22 +0530

    updated try block

--- dataSavers/creditCard/writeCardData.js
+++ dataSavers/creditCard/writeCardData.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 function writeCard(fileName, fileData) {
   try {
     fs.writeFileSync(fileName, JSON.stringify(data, null, 4));
-    console.log("file Written Successfully");
   } catch (error) {
     console.log("Error Writting File");
   }
